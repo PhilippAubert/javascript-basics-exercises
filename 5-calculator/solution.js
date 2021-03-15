7025c55eb08b920744696b7809c0a0233f9a2676
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-03-15 23:57:18 +0100

    branch used for changes now

--- 5-calculator/solution.js
+++ 5-calculator/solution.js
@@ -3,7 +3,7 @@ let operand1 = Number(prompt("Pick a number or exit"));
 let operand2 = Number(prompt("Pick another number or exit"));
 
 // not sure about here
-let result = Number(operand1) + Number(operand2); //<- what is it here?
+let result = operand1 + operator + operand2; //<- what is it here?
 
 if (operator === "exit" || operand1 === "exit" || operand2 === "exit") {
   alert("You left the program");
